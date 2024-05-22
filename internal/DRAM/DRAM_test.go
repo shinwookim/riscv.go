8a7885a14b56b87c12c90066068c6b581e3e6227
Shinwoo Kim <shinwookim@pitt.edu>
2024-05-22 17:18:44 -0400

    test: add additional tests for DRAM API

--- internal/DRAM/DRAM_test.go
+++ internal/DRAM/DRAM_test.go
@@ -5,10 +5,6 @@ import (
 	"testing"
 )
 
-func randRange(min, max uint64) uint64 {
-	return uint64(rand.Int63n(int64(max-min)) + int64(min))
-}
-
 func TestDRAM(t *testing.T) {
 	var testMemory DRAM
 	err := Store(&testMemory, BaseAddr, 8, 15)
@@ -28,7 +24,7 @@ func TestDRAMSingleByte(t *testing.T) {
 	var testMemory DRAM
 	// Generate Random Address-Data Pairs
 	m := make(map[uint64]uint64)
-	for i := BaseAddr; i < BaseAddr+Size; i++ {
+	for i := BaseAddr; i+8 < BaseAddr+Size; i++ {
 		m[i] = rand.Uint64()
 		Store(&testMemory, i, 8, m[i])
 	}
@@ -49,7 +45,7 @@ func TestDRAMTwoBytes(t *testing.T) {
 	// Generate Random Address-Data Pairs
 	m := make(map[uint64]uint64)
 	var topAddr uint64 = BaseAddr
-	for ; topAddr < BaseAddr+Size; topAddr += 2 {
+	for ; topAddr+16 < BaseAddr+Size; topAddr += 2 {
 		m[topAddr] = rand.Uint64()
 		Store(&testMemory, topAddr, 16, m[topAddr])
 	}
@@ -68,7 +64,7 @@ func TestDRAMFourBytes(t *testing.T) {
 	// Generate Random Address-Data Pairs
 	m := make(map[uint64]uint64)
 	var topAddr uint64 = BaseAddr
-	for ; topAddr < BaseAddr+Size; topAddr += 4 {
+	for ; topAddr+32 < BaseAddr+Size; topAddr += 4 {
 		m[topAddr] = rand.Uint64()
 		Store(&testMemory, topAddr, 32, m[topAddr])
 	}
@@ -87,7 +83,7 @@ func TestDRAMEightBytes(t *testing.T) {
 	// Generate Random Address-Data Pairs
 	m := make(map[uint64]uint64)
 	var topAddr uint64 = BaseAddr
-	for ; topAddr < BaseAddr+Size; topAddr += 8 {
+	for ; topAddr+64 < BaseAddr+Size; topAddr += 8 {
 		m[topAddr] = rand.Uint64()
 		Store(&testMemory, topAddr, 64, m[topAddr])
 	}
@@ -102,4 +98,43 @@ func TestDRAMEightBytes(t *testing.T) {
 	}
 }
 
-func TestDRAMRandBytes(t *testing.T) {}
+func TestDRAMRandBytes(t *testing.T) {
+
+	var testMemory DRAM
+	blockSizes := []uint64{8, 16, 32, 64}
+	type sizedData struct {
+		size uint64
+		data uint64
+	}
+	m := make(map[uint64]sizedData)
+	var addr uint64 = BaseAddr
+	for {
+		if addr >= BaseAddr+Size-64 {
+			break
+		}
+		m[addr] = sizedData{blockSizes[rand.Intn(len(blockSizes))], rand.Uint64()}
+		Store(&testMemory, addr, m[addr].size, m[addr].data)
+		addr += (m[addr].size / 8)
+	}
+
+	for address, val := range m {
+		data, err := Load(&testMemory, address, val.size)
+		if err != nil {
+			t.Fatalf("Error while loading: %v", err)
+		}
+		var expectedData uint64
+		switch val.size {
+		case 8:
+			expectedData = uint64(uint8(val.data))
+		case 16:
+			expectedData = uint64(uint16(val.data))
+		case 32:
+			expectedData = uint64(uint32(val.data))
+		case 64:
+			expectedData = val.data
+		}
+		if data != expectedData {
+			t.Errorf("Wrong data: %d\t Expected %d at address %d", data, expectedData, address)
+		}
+	}
+}
